c87ba81966cb840f046c5f94fab51cbdc52580fb
PortellaTheSelincourt <portell@hotmail.co.uk>
2020-11-24 18:24:58 +0000

    Enhanced this MS to return a Bean HelloWorldBean
    Signed-off-by: PortellaTheSelincourt <portell@hotmail.co.uk>

---
+++ src/main/java/com/udemy/rest/webservices/restfulwebservices/HelloWorldController.java
@@ -0,0 +1,21 @@
+package com.udemy.rest.webservices.restfulwebservices;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+//Controller
+@RestController
+public class HelloWorldController {
+
+    @GetMapping(path = "/hello-world")
+    public String helloWorld() {
+        return "Hello World!";
+
+    }
+
+    @GetMapping(path = "/hello-world-bean")
+    public HelloWorldBean helloWorldBean() {
+        return new HelloWorldBean("Hello World!");
+    }
+
+}
