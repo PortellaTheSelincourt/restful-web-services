2b81d39a3f513bd03b253c9664dea5ae65696f44
PortellaTheSelincourt <portell@hotmail.co.uk>
2020-11-26 12:58:05 +0000

    Enhanced POST method to return correct HTTP status code and location and implemented Exception handling 404 resource not found
    Signed-off-by: PortellaTheSelincourt <portell@hotmail.co.uk>

--- src/main/java/com/udemy/rest/webservices/restfulwebservices/user/UserResource.java
+++ src/main/java/com/udemy/rest/webservices/restfulwebservices/user/UserResource.java
@@ -1,8 +1,12 @@
 package com.udemy.rest.webservices.restfulwebservices.user;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
+import java.nio.file.attribute.UserPrincipalNotFoundException;
 import java.util.List;
 
 @RestController
@@ -12,17 +16,25 @@ public class UserResource {
     private UserDaoService service;
 
     @GetMapping("/users")
-    public List<User> retrieveAllUsers(){
-        return  service.findAll();
+    public List<User> retrieveAllUsers() {
+        return service.findAll();
     }
 
     @GetMapping(path = "/users/{id}")
-    public User retrieveUser(@PathVariable int id){
-        return  service.findOne(id);
-    }
-    @PostMapping("/users")
-    public void createUser(@RequestBody User user){
-        User savedUser = service.save(user);
+    public User retrieveUser(@PathVariable int id) {
+        User user = service.findOne(id);
+        if (user == null)
+            throw new UserNotFoundException("id-" + id);
+        return user;
     }
 
+    @PostMapping("/users")
+    public ResponseEntity<Object> createUser(@RequestBody User user) {
+        User savedUser = service.save(user);
+        URI location = ServletUriComponentsBuilder
+                .fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(savedUser.getId()).toUri();
+       return ResponseEntity.created(location).build();
+    }
 }
